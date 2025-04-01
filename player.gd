6fec224cb821bb7fb82fe0e20d01cf6febf06217
David Urbina <davidurbina.dev@gmail.com>
2025-03-31 20:34:31 -0500

    feat: create parallax background

--- player.gd
+++ player.gd
@@ -1,22 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var _speed = 300.0
+@export var _jump_velocity = -400.0
 
 
 func _ready() -> void:
 	# We'll always move to the right
-	#velocity = Vector2.RIGHT * SPEED
-	pass
+	velocity = Vector2.RIGHT * _speed
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#velocity += get_gravity() * delta
+	velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
+		velocity.y = _jump_velocity
 
 	move_and_slide()
