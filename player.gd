613332fbc9a83c58b5b4cf763dfc06493419200c
David Urbina <davidurbina.dev@gmail.com>
2025-03-31 21:26:39 -0500

    feat: create base obstacle scenes

--- player.gd
+++ player.gd
@@ -2,20 +2,21 @@ extends CharacterBody2D
 
 
 @export var _speed = 300.0
-@export var _jump_velocity = -400.0
+@export var _jump_velocity = 400.0
 
 
 func _ready() -> void:
 	# We'll always move to the right
-	velocity = Vector2.RIGHT * _speed
+	#velocity = Vector2.RIGHT * _speed
+	pass
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	velocity += get_gravity() * delta
+	#velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = _jump_velocity
+		velocity.y = _jump_velocity * -1
 
 	move_and_slide()
