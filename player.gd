7be2cef91a9de199c758900be7028de584697038
David Urbina <davidurbina.dev@gmail.com>
2025-04-11 19:51:45 -0500

    implement fake parallax and player collision

--- player.gd
+++ player.gd
@@ -1,22 +1,56 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
-@export var _speed = 300.0
-@export var _jump_velocity = 400.0
+@export var _jump_velocity = 200.0
 
+var _state = States.ALIVE
+enum States {
+	ALIVE,
+	DYING,
+	DEAD,
+}
 
 func _ready() -> void:
-	# We'll always move to the right
-	#velocity = Vector2.RIGHT * _speed
-	pass
+	SignalBus.player_hit.connect(_on_player_hit)
+	_animated_sprite_2d.animation_finished.connect(_on_animation_finished)
 
 
 func _physics_process(delta: float) -> void:
+	match _state:
+		States.DYING:
+			_die(delta)
+		States.DEAD:
+			return
+		_:
+			pass
+
 	# Add the gravity.
-	#velocity += get_gravity() * delta
+	velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	var jump_strength := Input.get_action_strength(&"jump")
+	if (jump_strength):
 		velocity.y = _jump_velocity * -1
 
 	move_and_slide()
+
+
+func _on_player_hit() -> void:
+	_state = States.DYING
+
+
+func _on_animation_finished() -> void:
+	if _animated_sprite_2d.animation == "die":
+		_state = States.DEAD
+
+
+func _die(delta: float) -> void:
+	if not _animated_sprite_2d.animation == "die":
+		_animated_sprite_2d.animation = "die"
+
+	var target_y := 90.0
+	var distance := target_y - global_position.y
+	var lerp_speed := 0.1
+
+	velocity.y = lerp(velocity.y, distance / delta, lerp_speed)
